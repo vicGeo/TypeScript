c22ee95294e3f82fce8d7da0a39f37530b4edf9e
vicGeo <vicsanig@alumni.upv.es>
2021-05-25 23:29:08 +0200

    added DataFetching component

---
+++ src/components/DataFetching.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./DataFetching.css";
+
+function DataFetching() {
+  return <></>;
+}
+
+export default DataFetching;
