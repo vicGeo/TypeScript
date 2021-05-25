2dc76e7f02491c213b5c1472bf003ce041d938c6
vicGeo <vicsanig@alumni.upv.es>
2021-05-25 23:33:21 +0200

    interface IPost

--- src/components/DataFetching.tsx
+++ src/components/DataFetching.tsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./DataFetching.css";
 
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 function DataFetching() {
   return <></>;
 }
